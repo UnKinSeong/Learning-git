40a0147e1d03bcb8dae950638a4bf32af23277ef
Stanley Un <stanley09537000@gmail.com>
2022-02-21 10:17:01 -0500

    Modify the main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,23 @@
+<<<<<<< HEAD
+#include <cstdio>
+#include <stdlib.h>
+#include <iostream>
+#include <chrono>
+#include <queue>
+#include <vector>
+#include <unordered_map>
+#include <mutex>
+#include <condition_variable>
+
+int main(int argc, char *argv[]) {
+    std::vector<int> fv;
+    for(int i = 0; i<100;i++){
+        fv.push_back(i);
+    }
+    for(auto i : fv){
+        printf("str %d",i);
+    }
+=======
 #include <iostream>
 
 
@@ -5,5 +25,6 @@
 int main(int argc, char *argv[]) {
 
     std::cout<< "Something" << std::endl;
+>>>>>>> cd85386 (Add a c++ simple program)
     return 0;
 };
